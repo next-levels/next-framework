13fe31876a4b72f852d7e2d6ffd81d56ea0e4cb5
Roland Kirchhoff <roland.kirchhoff@next-levels.de>
2023-09-26 16:15:44 +0200

    Fix: release with tsc: VMMS-154

--- src/lib/packages/backend/generic-api/src/lib/types/controller.type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseApiController = void 0;
-var BaseApiController = /** @class */ (function () {
-    function BaseApiController() {
-    }
-    return BaseApiController;
-}());
-exports.BaseApiController = BaseApiController;
--- src/lib/packages/backend/generic-api/src/lib/types/service.type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseApiService = void 0;
-var BaseApiService = /** @class */ (function () {
-    function BaseApiService() {
-    }
-    return BaseApiService;
-}());
-exports.BaseApiService = BaseApiService;
