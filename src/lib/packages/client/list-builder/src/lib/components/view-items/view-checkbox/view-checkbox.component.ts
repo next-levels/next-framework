7bc469685faf092cf460d9eea95ade194e30cdae
Roland Kirchhoff <roland.kirchhoff@next-levels.de>
2024-03-13 11:24:12 +0100

    Fix: list builder checkbox

--- src/lib/packages/client/list-builder/src/lib/components/view-items/view-checkbox/view-checkbox.component.ts
+++ src/lib/packages/client/list-builder/src/lib/components/view-items/view-checkbox/view-checkbox.component.ts
@@ -3,17 +3,23 @@ import { BaseViewComponent } from '../base-view/base-view.component';
 
 @Component({
   selector: 'next-view-checkbox',
-   template: `
-     @if (this._value) {
-       <mat-icon class="text-green-400 green-400" [svgIcon]="'heroicons_outline:check'"></mat-icon>
-     } @else {
-        <mat-icon class="text-gray-400 gray-400" [svgIcon]="'heroicons_outline:x-mark'"></mat-icon>
-     }
-    `
+  template: `
+    @if (this._value) {
+    <mat-icon
+      class="text-green-400 green-400"
+      [svgIcon]="'heroicons_outline:check'"
+    ></mat-icon>
+    } @else {
+    <mat-icon
+      class="text-gray-400 gray-400"
+      [svgIcon]="'heroicons_outline:x-mark'"
+    ></mat-icon>
+    }
+  `,
 })
 export class ViewCheckboxComponent extends BaseViewComponent implements OnInit {
-  override ngOnInit() {
-    super.ngOnInit();
-    this._value = this._value ? 'Ja' : 'Nein';
-  }
+  // override ngOnInit() {
+  //   super.ngOnInit();
+  //   this._value = this._value ? 'Ja' : 'Nein';
+  // }
 }
