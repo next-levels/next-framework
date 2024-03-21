1cb3fc0332c8d5f87d450624ad197f137800490a
Slawa Ditzel <slawa.ditzel@next-levels.de>
2024-03-21 11:56:46 +0100

    (no branch): style💎[view-checkbox.component]: Improve code readability
    Adjusted the indentation in the view-checkbox component to enhance code readability and maintainability. No functional changes were made.

--- src/lib/packages/client/list-builder/src/lib/components/view-items/view-checkbox/view-checkbox.component.ts
+++ src/lib/packages/client/list-builder/src/lib/components/view-items/view-checkbox/view-checkbox.component.ts
@@ -5,15 +5,15 @@ import { BaseViewComponent } from '../base-view/base-view.component';
   selector: 'next-view-checkbox',
   template: `
     @if (this._value) {
-    <mat-icon
-      class="text-green-400 green-400"
-      [svgIcon]="'heroicons_outline:check'"
-    ></mat-icon>
+      <mat-icon
+        class="text-green-400 green-400"
+        [svgIcon]="'heroicons_outline:check'"
+      ></mat-icon>
     } @else {
-    <mat-icon
-      class="text-gray-400 gray-400"
-      [svgIcon]="'heroicons_outline:x-mark'"
-    ></mat-icon>
+      <mat-icon
+        class="text-gray-400 gray-400"
+        [svgIcon]="'heroicons_outline:x-mark'"
+      ></mat-icon>
     }
   `,
 })
